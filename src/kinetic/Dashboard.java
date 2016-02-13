89237b26b05a1cd6b01fe2ee847d0edad70a24e1
Katie V <bog02@gmail.com>
2016-02-12 17:36:13 -0800

    Added Dashboard stuff

--- src/kinetic/Dashboard.java
+++ src/kinetic/Dashboard.java
@@ -1,5 +1,15 @@
 package kinetic;
 
-public class Dashboard {
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
+public class Dashboard {
+	static NetworkTable networkTable;
+
+	public static void init() {
+		networkTable = NetworkTable.getTable("table");
+	}
+
+	public static void run() {
+		networkTable.putNumber("table", 1);
+	}
 }
