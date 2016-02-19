ac3876c8d5ad6b271e538b8dc5069ac12bdb60aa
Henry Choi <googolydox@gmail.com>
2016-02-19 11:46:37 -0800

    Updated CAN IDs
    21,22,23,24 for CANTalon

--- src/kinetic/RM.java
+++ src/kinetic/RM.java
@@ -12,12 +12,11 @@ public class RM {
     
     
 	public static void init() {
-		frontLeft = new CANTalon(1);
-		frontRight = new CANTalon (2);
-		rearRight= new CANTalon(3);
-		rearLeft = new CANTalon(4);
-		myDrive = new RobotDrive(frontLeft, rearRight, frontLeft, frontRight);
+		frontLeft = new CANTalon(21);
+		frontRight = new CANTalon (22);
+		rearRight= new CANTalon(23);
+		rearLeft = new CANTalon(24);
+		myDrive = new RobotDrive(frontLeft, frontRight, rearLeft, rearRight);
 		
-			
 	}
 }
