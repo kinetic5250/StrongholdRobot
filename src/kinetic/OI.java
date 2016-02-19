f5572f0c9b93229a0f582d0670b1dca385cc20b7
Henry Choi <googolydox@gmail.com>
2016-02-19 13:38:50 -0800

    Added USB Camera

--- src/kinetic/OI.java
+++ src/kinetic/OI.java
@@ -1,15 +1,19 @@
 package kinetic;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Joystick;
 
-/**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
- */
 public class OI {
-	Joystick stick1;
+	public static Joystick stick1;
+    public static CameraServer server;
+    
     public static void init() {
+		stick1 = new Joystick(0);
 		
+		server = CameraServer.getInstance();
+        server.setQuality(50);
+        //the camera name (ex "cam0") can be found through the roborio web interface
+        server.startAutomaticCapture("cam0");
     };
 }
 
