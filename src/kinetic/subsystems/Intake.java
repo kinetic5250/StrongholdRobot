eb4cf43795eca3ca44e91f75be16c81d0794fca8
Henry Choi <googolydox@gmail.com>
2016-03-02 16:19:41 -0800

    Set IntakeCommand as not default

--- src/kinetic/subsystems/Intake.java
+++ src/kinetic/subsystems/Intake.java
@@ -1,12 +1,10 @@
 package kinetic.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import kinetic.commands.IntakeCommand;
 
 public class Intake extends Subsystem {
 	
 	public void initDefaultCommand() {
-		setDefaultCommand(new IntakeCommand());
 	}
 	
 	
