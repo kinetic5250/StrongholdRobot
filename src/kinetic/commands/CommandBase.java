32c33cd6358f643942c170e5f9f3ad8738d7432a
Don Noxon <don_noxon@hotmail.com>
2016-03-17 16:04:01 -0700

    Remove intake command

--- src/kinetic/commands/CommandBase.java
+++ src/kinetic/commands/CommandBase.java
@@ -1,8 +1,6 @@
 package kinetic.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import kinetic.subsystems.Intake;;
 
 public abstract class CommandBase extends Command {
-	Intake intake = new Intake();
 }
